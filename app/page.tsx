0d08757849b3509b5a9d69fc9d636556ff2f5cd1
Thomas Hartmann <thomassafezone@gmail.com>
2023-11-05 23:48:09 +0100

    .

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { HeroSection } from "./components/pages/home/hero-section";
 export default async function Home() {
   return (
     <>
-      <HeroSection/>
+      <HeroSection />
     </>
   )
 }
