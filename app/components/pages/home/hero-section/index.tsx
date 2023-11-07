b76a807e1a9835cef99b54d24df1fd744c0afc5c
Thomas Hartmann <thomassafezone@gmail.com>
2023-11-07 01:07:12 +0100

    HeroSection working full with CMS

--- app/components/pages/home/hero-section/index.tsx
+++ app/components/pages/home/hero-section/index.tsx
@@ -1,26 +1,17 @@
 'use client'
 import { Button } from '@/app/components/button'
+import { CMSIcon } from '@/app/components/cms-icon'
+import { RichText } from '@/app/components/rich-text'
 import { TechBadge } from '@/app/components/tech-badge'
+import { HomePageInfo } from '@/app/types/page-info'
 import Image from 'next/image'
 import { HiArrowNarrowRight } from 'react-icons/hi'
-import { TbBrandGithub, TbBrandWhatsapp, TbBrandLinkedin} from 'react-icons/tb'
 
-const MOCK_CONTACTS = [
-    {
-    url: 'https://github.com/ThomasHartmannDev',
-    icon: <TbBrandGithub/>
-    },
-    {   
-    url: 'https://github.com/ThomasHartmannDev',
-    icon: <TbBrandLinkedin/>
-    },
-    {
-    url: 'https://github.com/ThomasHartmannDev',
-    icon: <TbBrandWhatsapp/>
-    }
-]
+type HomeSectionProps = {
+    homeInfo: HomePageInfo;
+}
 
-export const HeroSection = () => {
+export const HeroSection = ({homeInfo}: HomeSectionProps) => {
     const handleContact = () =>{
         const contactSection = document.querySelector('#contact');
         if(contactSection){
@@ -33,10 +24,12 @@ export const HeroSection = () => {
                 <div className='w-full lg:max-w-[530px]'>
                     <p className='text-mono text-purple-400'>Olá, meu nome é</p>
                     <h1 className='text-4xl font-medium mt-2'>Thomas Hartmann</h1>
-                    <p className='text-gray-400 my-6 text-sm sm:text-base'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <div className='text-gray-400 my-6 text-sm sm:text-base'>
+                        <RichText content={homeInfo.introduction.raw}/>
+                    </div>
                     <div className='flex flex-wrap gap-x-2 gap-y-3 lg:max-w-[340px]'>
-                        {Array.from({length: 5}).map((_, index) =>(
-                            <TechBadge name = 'next.js'/>
+                        {homeInfo.technologies.map((tech, i) =>(
+                            <TechBadge name = {tech.name}/>
                         ))}
                     </div>
                     <div className='mt-6 lg:mt10 flex sm:items-center sm:gap-5 flex-col sm:flex-row'>
@@ -45,14 +38,14 @@ export const HeroSection = () => {
                             <HiArrowNarrowRight size={18}/> 
                         </Button>
                         <div className='text-gray-600 text-2xl flex items-center h-20 gap-3'>
-                            {MOCK_CONTACTS.map((contact, index) =>(
+                            {homeInfo.socials.map((contact, index) =>(
                                 <a 
                                     href={contact.url} 
                                     key={`contact-${index}`}
                                     target='_blank'
                                     className='hover:text-gray-100 transition-colors'
                                 >
-                                    {contact.icon}
+                                    <CMSIcon icon={contact.iconSvg}/>
                                 </a>
                             ))}
                         </div>
@@ -60,7 +53,7 @@ export const HeroSection = () => {
                     </div>
                 </div>
 
-                <Image width={420} height={404} src='/images/profile-pic.png' alt='Foto de perfil do Thomas Hartmann'
+                <Image width={420} height={404} src={homeInfo.profilePicture.url} alt='Foto de perfil do Thomas Hartmann'
                     className='w-[300px] h-[300px] lg:w-[420px] lg:h-[404px] mb-6 lg:mb-0 shadow-2xl rounded-lg object-cover'
                 />
             </div>
--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,45 @@ import { HeroSection } from '@/app/components/pages/home/hero-section'
 import { KnownTechs } from './components/pages/home/known-techs'
 import { HighlightedProjects } from './components/pages/home/highlighted-projects'
 import { WorkExperience } from './components/pages/home/workExperience'
-import { ContactForm } from './components/contact-forms'
+import { fetchHygraphQuery } from './utils/fetch-hygraph-query'
+import { HomePageData } from './types/page-info'
+
+const getPageData = async ():Promise<HomePageData> =>{
+    const query = `
+    query PageInfoQuery {
+      page(where: {slug: "home"}) {
+        introduction {
+          raw
+        }
+        technologies {
+          name
+        }
+        profilePicture {
+          url
+        }
+        socials {
+          url
+          iconSvg
+        }
+        knownTechs {
+          iconSvg
+          name
+          startDate
+        }
+      }
+    }
+  `
+    return fetchHygraphQuery(query, 60 * 60 * 24);
+}
 
 export default async function Home() {
+  const { page: pageData } = await getPageData();
+  
   return (
     <>
-      <HeroSection />
+      <HeroSection homeInfo = {pageData}/>
       <KnownTechs />
       <HighlightedProjects />
-      <WorkExperience />
       
     </>
   )
